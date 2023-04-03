57e0ed2ecdd3b83a9faa945808639c928ac3ec9d
Emmanuel Adewoye <Emmanuel>
2023-04-03 20:00:48 +0100

    feat: added the footer social media icons

--- src/Component/Icons.js
+++ src/Component/Icons.js
@@ -1,18 +1,24 @@
-import '../Assets/styles/home.css'
-
-import email from '../Assets/images/email.png'
-import facebook from '../Assets/images/facebook.png'
-import twitter from '../Assets/images/twitter.png'
-import github from '../Assets/images/github.png'
+import "../Assets/styles/icons.css"
 
 const Icons = () => {
   return (
-    <div className="footer-icons">
-        <a href='https://twitter.com/_Emmaxfans' target="_blank" rel='noreferrer'><img src={twitter} alt="twitter" className="img-footer"/></a>
-        <a href='https://facebook.com' target="_blank" rel='noreferrer'><img src={facebook} alt="facebook" className="img-footer"/></a>
-        <a href='https://github.com/Emmaxfans' target="_blank" rel='noreferrer'><img src={github} alt="github" className="img-footer"/></a>
-        <a href='mailto:Emmaxfans@gmail.com' target="_blank" rel='noreferrer'><img src={email} alt="email" className="img-footer"/></a>
-    </div>
+    <div className="social-icons footer-icons" id="contact">
+    <ul>
+        <li>
+            <a href='https://twitter.com/_Emmaxfans' target="_blank" rel='noreferrer'><i class="fab fa-twitter glow"/></a>
+        </li>
+        <li>
+            <a href='https://github.com/Emmaxfans' target="_blank" rel='noreferrer'><i class="fab fa-github glow"/></a>
+        </li>
+        <li>
+            <a href='/#' target="_blank" rel='noreferrer'><i class="fab fa-linkedin-in glow"/></a>
+        </li>
+        <li>
+            <a href='/#' target="_blank" rel='noreferrer'><i class="fab fa-instagram glow1"/></a>
+        </li>
+    </ul>
+  
+</div>
   )
 }
 
